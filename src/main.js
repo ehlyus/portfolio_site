582778e6dcb781daa36c9d1d1b214533e3a7e9f2
Elijah Johnson <ejohnson@pioneeringevolution.com>
2024-02-24 19:43:11 -0500

    convert to svelete app

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap-trial/ScrollSmoother';
+
+import './app.css';
+import App from './App.svelte';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const app = new App({
+	target: document.body,
+});
+
+export default app;
