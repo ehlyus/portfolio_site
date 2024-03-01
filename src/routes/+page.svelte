7c39329d8b24e692cc352c4592497b359f1f893c
Elijah Johnson <johnsonelijah2626@gmail.com>
2024-03-01 15:10:28 -0500

    remove style tags on test boxes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,9 +58,9 @@
             <h1 class="title">Elijah Johnson's Personal Website</h1>
             <button class="button" on:click={scrollTo}> Jump to C</button>
         </header>
-        <div class="box box-a" style="border: 1px solid yellow;" data-speed="1">a</div>
-        <div class="box box-b" style="border: 1px solid yellow;" data-speed="1">b</div>
-        <div class="box box-c" style="border: 1px solid yellow;" data-speed="1">c</div>
+        <div class="box box-a" data-speed="1">a</div>
+        <div class="box box-b" data-speed="1">b</div>
+        <div class="box box-c" data-speed="1">c</div>
         <div class="line"/>
     </div>
 </div>
