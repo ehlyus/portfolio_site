ffe0ce5028de8dcf1ea9e69a8fa4d7cbae6b16b4
Elijah Johnson <johnsonelijah2626@gmail.com>
2024-03-03 12:12:49 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,7 @@
     <title>Elijah Johnson - Personal Website</title>
     <meta name="description" content="SvelteKit portfolio site"/>
 </svelte:head>
-
-<div id="smooth-wrapper" bind:this={main}>
-    <div id="smooth-content">
-    </div>
-</div>
+<Gallery />
 <footer>
 
 </footer>
@@ -14,27 +10,14 @@
 <script lang="ts">
     import {onMount} from "svelte";
     import {gsap} from "gsap";
-    import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
-    import {ScrollSmoother} from "gsap/dist/ScrollSmoother";
+    import Gallery from "../components/Gallery.svelte";
 
     /**
      * @type {globalThis.ScrollSmoother}
      */
-    let main: any, smoother: ScrollSmoother;
-
-    const scrollTo = () => {
-        smoother
-    };
+    let main: any
 
     onMount(() => {
-        const ctx = gsap.context(() => {
-            smoother = ScrollSmoother.create({
-                smooth: 2, // seconds it takes to "catch up" to native scroll position
-                effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
-            });
-
-        }, main); // <- Scope!
-
-        return () => ctx.revert(); // <- Cleanup!
+        return;
     });
 </script>
