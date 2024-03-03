f0f59eb4d5676a8ca74310737988f1c38750d508
Elijah Johnson <johnsonelijah2626@gmail.com>
2024-03-03 12:13:20 -0500

    pull model in from assets and render model

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -4,44 +4,36 @@
     import { onMount } from 'svelte';
     import { gsap } from 'gsap';
     import * as THREE from 'three';
-    import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+    import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+    import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
     let galleryContainer;
 
     onMount(() => {
-        // Initialize Three.js scene
         const scene = new THREE.Scene();
+
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         galleryContainer.appendChild(renderer.domElement);
 
-        // Load the Sketchfab model
+        const controls = new OrbitControls( camera, renderer.domElement);
+
         const loader = new GLTFLoader();
-        loader.load('path/to/your/model.gltf', (gltf) => {
+        loader.load('src/assets/art_gallery__zen__baked/scene.gltf', (gltf) => {
+            gltf.scene.position.set(0,-1,-4)
             scene.add(gltf.scene);
         });
 
-        // Set up GSAP scroll trigger or any other animations
-        // Example:
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: galleryContainer,
-                start: 'top top',
-                end: 'bottom bottom',
-                scrub: true
-            }
-        });
-        // Define animations
-        tl.to(camera.position, {
-            duration: 1,
-            z: -10, // Move the camera forward
-            ease: 'power1.inOut'
-        });
+        camera.position.z = 0;
+        controls.enableZoom = true;
+        controls.enablePan = true;
+        controls.enableRotate = true;
 
         // Animation loop
         const animate = () => {
             requestAnimationFrame(animate);
+            controls.update();
             renderer.render(scene, camera);
         };
         animate();
