e91b1efec8584a9dcc18a1f8aa63ebf34908f3f2
Elijah Johnson <johnsonelijah2626@gmail.com>
2024-03-05 00:27:00 -0500

    fixes lighting

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,94 +1,124 @@
-<!-- Gallery.svelte -->
 <script lang="ts">
     // @ts-nocheck
     import { onMount } from 'svelte';
     import * as THREE from 'three';
-    import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
     import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+    import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+    import { Sky } from "three/examples/jsm/objects/Sky";
+    import GUI from "three/examples/jsm/libs/lil-gui.module.min";
 
     let galleryContainer;
     let cameraPosition = { x: 0, y: 0, z: 0 };
     let targetPosition = { x: 0, y: 0, z: 0 };
-    let camera;
+    let camera, scene, renderer;
+    let sky, sun;
     let controls;
     const positionToString = (position) => {
         return `x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(2)}, z: ${position.z.toFixed(2)}`;
     };
 
+    function initSky() {
+        sky = new Sky();
+        sky.scale.setScalar( 450000 );
+        scene.add( sky );
+
+        sun = new THREE.Vector3();
+
+        const effectController = {
+            turbidity: 10,
+            rayleigh: 3,
+            mieCoefficient: 0.005,
+            mieDirectionalG: 0.7,
+            elevation: 2,
+            azimuth: 180,
+            exposure: renderer.toneMappingExposure
+        };
+
+        function guiChanged() {
+            const uniforms = sky.material.uniforms;
+            uniforms[ 'turbidity' ].value = effectController.turbidity;
+            uniforms[ 'rayleigh' ].value = effectController.rayleigh;
+            uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
+            uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+
+            const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
+            const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+
+            sun.setFromSphericalCoords( 1, phi, theta );
+
+            uniforms[ 'sunPosition' ].value.copy( sun );
+
+            renderer.toneMappingExposure = effectController.exposure;
+            renderer.render( scene, camera );
+        }
+
+        const gui = new GUI();
+
+        gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
+        gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
+        gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
+        gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
+        gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
+        gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
+        gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
+
+        guiChanged();
+    }
+
     onMount(() => {
-        const renderer = new THREE.WebGLRenderer({ antialias: true });
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xffffff); // Set white background
-        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-        renderer.toneMapping = THREE.ReinhardToneMapping; // Adjust tone mapping algorithm
-        renderer.toneMappingExposure = 3; // Increase exposure for brighter scene
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        scene = new THREE.Scene();
+        const helper = new THREE.GridHelper( 10000, 2, 0xffffff, 0xffffff );
+        scene.add( helper );
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+        renderer.toneMapping = THREE.ReinhardToneMapping;
+        renderer.toneMappingExposure = 3;
         renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setPixelRatio(window.devicePixelRatio);
 
         galleryContainer.appendChild(renderer.domElement);
 
         controls = new OrbitControls(camera, renderer.domElement);
-        // Set initial camera position and target
-        camera.position.set(0.45, 1.25, 3.46);
-        controls.target.set(6.83, 1.26, -4.12);
-        controls.update()
+        camera.position.set(3.66, 0.83, 14.38);
+        controls.target.set(0.34, 2.56, -0.37);
+        controls.update();
 
         const loader = new GLTFLoader();
-        loader.load('src/assets/art_gallery__zen__baked/scene.gltf', (gltf) => {
+        loader.load('src/assets/untitled.glb', (gltf) => {
             scene.add(gltf.scene);
-        }, undefined, function ( error ) {
-            console.error( error );
-        } );
+        }, undefined, function (error) {
+            console.error('Error loading GLB file:', error);
+        });
 
-        // Add ambient light to brighten up the scene
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Increase intensity to brighten the scene
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         scene.add(ambientLight);
 
-        // Add directional light to simulate sunlight from above
-        const sunlight = new THREE.DirectionalLight(0xffffff, 5); // Increased intensity to 5
-        sunlight.position.set(0, 1, 0); // Set position above the scene
-        sunlight.castShadow = true; // Enable shadow casting
-        scene.add(sunlight);
+        const dirLight = new THREE.DirectionalLight(0xffffff, 5);
+        scene.add(dirLight);
 
-        // Set up shadow properties for the light
-        sunlight.shadow.mapSize.width = 8192; // Increase shadow map size for higher quality
-        sunlight.shadow.mapSize.height = 8192;
-        sunlight.shadow.camera.near = 0.5; // Near plane of the shadow camera
-        sunlight.shadow.camera.far = 100; // Far plane of the shadow camera
+        initSky();
 
-        // Set up shadow camera parameters for better quality
-        sunlight.shadow.camera.left = -40;
-        sunlight.shadow.camera.right = 40;
-        sunlight.shadow.camera.top = 40;
-        sunlight.shadow.camera.bottom = -40;
-
-        // Set shadow bias to reduce self-shadowing artifacts
-        sunlight.shadow.bias = -0.0005;
-
-        // Enable shadow receiving for objects
-        renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Soft shadows for smoother appearance
-
-        // Animation loop
         const animate = () => {
             requestAnimationFrame(animate);
-
-            // Update controls before rendering
             controls.update();
-            cameraPosition = { ...camera.position };
-            targetPosition = { ...controls.target };
-
-            // Position the orb at the controls target
-            // controlOrb.position.copy(controls.target);
-
-            renderer.setSize(window.innerWidth, window.innerHeight); // Adjust renderer size based on window size
-            camera.aspect = window.innerWidth / window.innerHeight; // Adjust camera aspect ratio
-            camera.updateProjectionMatrix(); // Update camera projection matrix
-
+            cameraPosition = camera.position.clone();
+            targetPosition = controls.target.clone();
             renderer.render(scene, camera);
         };
 
         animate();
+
+        const handleResize = () => {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     });
 </script>
 
