3079b3f69967cc7d5d5e42cb955a3490b01d40b9
Elijah Johnson <johnsonelijah2626@gmail.com>
2024-03-06 20:39:45 -0500

    Update Gallery.svelte
    clean up code, automatically trigger initial transition

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -2,29 +2,25 @@
     // @ts-nocheck
     import { onMount } from 'svelte';
     import * as THREE from 'three';
-    // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
     import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
     import { Sky } from "three/examples/jsm/objects/Sky";
     import GUI from "three/examples/jsm/libs/lil-gui.module.min";
     import { gsap } from 'gsap';
-    import {FirstPersonControls} from "three/examples/jsm/controls/FirstPersonControls";
-    import {Clock} from "three";
+    import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
 
     let galleryContainer;
     let cameraPosition = { x: 0, y: 0, z: 0 };
-    const clock = new Clock()
-    // let targetPosition = { x: 0, y: 0, z: 0 };
     let camera, scene, renderer;
     let sky, sun;
-    // let controls;
+    const clock = new THREE.Clock();
     const positionToString = (position) => {
         return `x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(2)}, z: ${position.z.toFixed(2)}`;
     };
 
     function initSky() {
         sky = new Sky();
-        sky.scale.setScalar( 450000 );
-        scene.add( sky );
+        sky.scale.setScalar(450000);
+        scene.add(sky);
 
         sun = new THREE.Vector3();
 
@@ -33,7 +29,7 @@
             rayleigh: 1.1,
             mieCoefficient: 0.005,
             mieDirectionalG: 0.7,
-            elevation: 8, // Adjust sun's elevation
+            elevation: 8,
             azimuth: 39,
             exposure: 2
         };
@@ -45,32 +41,32 @@
             uniforms.mieCoefficient.value = effectController.mieCoefficient;
             uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
 
-            const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
-            const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+            const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+            const theta = THREE.MathUtils.degToRad(effectController.azimuth);
 
-            sun.setFromSphericalCoords( 1, phi, theta );
+            sun.setFromSphericalCoords(1, phi, theta);
 
-            uniforms.sunPosition.value.copy( sun );
+            uniforms.sunPosition.value.copy(sun);
 
             renderer.toneMappingExposure = effectController.exposure;
-            renderer.render( scene, camera );
+            renderer.render(scene, camera);
         }
 
         const gui = new GUI();
 
-        gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
-        gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
-        gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
-        gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
-        gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
-        gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
-        gui.add( effectController, 'exposure', 0, 2, 0.0001 ).onChange( guiChanged ); // Adjusted maximum exposure
+        gui.add(effectController, 'turbidity', 0.0, 20.0, 0.1).onChange(guiChanged);
+        gui.add(effectController, 'rayleigh', 0.0, 4, 0.001).onChange(guiChanged);
+        gui.add(effectController, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(guiChanged);
+        gui.add(effectController, 'mieDirectionalG', 0.0, 1, 0.001).onChange(guiChanged);
+        gui.add(effectController, 'elevation', 0, 90, 0.1).onChange(guiChanged);
+        gui.add(effectController, 'azimuth', -180, 180, 0.1).onChange(guiChanged);
+        gui.add(effectController, 'exposure', 0, 2, 0.0001).onChange(guiChanged);
 
         guiChanged();
     }
 
     onMount(() => {
-        renderer = new THREE.WebGLRenderer({ antialias: true }); // Enable anti-aliasing
+        renderer = new THREE.WebGLRenderer({ antialias: true });
         scene = new THREE.Scene();
         camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
         renderer.toneMapping = THREE.ReinhardToneMapping;
@@ -80,59 +76,33 @@
 
         galleryContainer.appendChild(renderer.domElement);
 
-        // controls = new OrbitControls(camera, renderer.domElement);
-        camera.position.set( 3.98, 4.07, 11.79);
-        // controls.target.set(0.34, 2.56, -0.37);
-        // controls.update();
-        // const controls = new FirstPersonControls(camera, renderer.domElement);
-        // controls.movementSpeed = 8;
-        // controls.lookSpeed = 0.08;
+        camera.position.set(3.98, 4.07, 11.79);
 
         const loader = new GLTFLoader();
         loader.load('src/assets/untitled.glb', (model) => {
-            // Add the model to the scene
             scene.add(model.scene);
-            window.addEventListener('mouseup', () => {
-                // console.log(camera.position);
-                gsap.to(camera.position, {
-                    x: -4.5, y: 1.439498279696612, z: 7.5,
-                    duration: 3
-                });
-                gsap.to(camera.rotation, {
-                    x: 0,
-                    y: -.62,
-                    z: 0,
-                    duration: 3.3
-                })
-            });
+            modelLoadedCallback();
         }, undefined, function (error) {
             console.error('Error loading GLB file:', error);
         });
-        console.log(scene)
 
-        // Increase ambient light intensity
         const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
         scene.add(ambientLight);
 
-        // Adjust directional light intensity and position
         const dirLight = new THREE.DirectionalLight(0xffffff, 2);
         dirLight.position.set(5, 10, 5);
         dirLight.castShadow = true;
         scene.add(dirLight);
 
-        // Set up shadow properties for directional light
         dirLight.shadow.mapSize.width = 2048;
         dirLight.shadow.mapSize.height = 2048;
         dirLight.shadow.camera.near = 0.1;
         dirLight.shadow.camera.far = 50;
 
-        // Change shadow map type for smoother shadows
-
-        // Enable ambient occlusion
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Soften shadow edges
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
         initSky();
 
@@ -140,15 +110,32 @@
             requestAnimationFrame(animate);
             cameraPosition = camera.position.clone();
             renderer.render(scene, camera);
-            // controls.update(clock.getDelta());
         };
 
         animate();
+
         const handleResize = () => {
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
             renderer.setSize(window.innerWidth, window.innerHeight);
         };
+
+        const modelLoadedCallback = () => {
+            setTimeout(() => {
+                gsap.to(camera.position, {
+                    x: -4.5,
+                    y: 1.35,
+                    z: 7.5,
+                    duration: 3
+                });
+                gsap.to(camera.rotation, {
+                    x: 0,
+                    y: -.62,
+                    z: 0,
+                    duration: 3.1
+                })
+            }, 650)
+        }
 
         window.addEventListener('resize', handleResize);
 
@@ -159,11 +146,8 @@
 </script>
 
 <div class="gallery-container" bind:this={galleryContainer}>
-    <!--    <p style="color: white; float: left;"> Target Position: <span style="color: red;">{positionToString(targetPosition)} |</span></p>-->
     <p style="color: white; float: left;"> Camera Position: <span style="color: red;"> {positionToString(cameraPosition)}</span></p>
-    <!-- HTML markup -->
-    <button id="animateButton">Animate Suspended Structure</button>
-
+<!--    <button id="animateButton">Animate Suspended Structure</button>-->
 </div>
 
 <style>
